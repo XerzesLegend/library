eda524774ebba31f4315b99669f74c3355eb8cb0
Yosif <yousufalkhateeb@gmail.com>
2022-01-17 22:33:49 -0700

    added object constructor

---
+++ app.js
@@ -0,0 +1,15 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        if(read){
+            return(title + " by " + author + ", " + pages + "pages, read");
+        }
+        else{
+            return(title + " by " + author + ", " + pages + " pages, not read yet");
+        }
+    }
+
+}
